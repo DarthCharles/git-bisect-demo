5b8763c87a09a948c2dfad05e6b82f11baf8dc2a
ccontreras <ccontreras@forhims.com>
2021-06-09 20:02:40 -0700

    Version 0.0.100

--- src/App.tsx
+++ src/App.tsx
@@ -26,7 +26,7 @@ export default function App() {
         />
       </div>
       <div className="absolute bottom-0">
-        Version 0.0.99
+        Version 0.0.100
       </div>
     </div>
   );
